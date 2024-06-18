9344e264c9e19b425127c4733153a08d718d0bbf
Eugene Gatsak <greenglaz@gmail.com>
2024-06-18 20:42:06 +0200

    feat: implement get lambdas db requests

--- product-service/lib/handlers/getProductById.ts
+++ product-service/lib/handlers/getProductById.ts
@@ -1,21 +1,54 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { products } from "../db/db";
 import { buildResponse } from "../utils";
 import { HttpError } from "../errorHandler";
+import { MyQueryCommandOutput } from "../db/types";
+import { PopulatedProduct, Product, Stock } from "../types";
+import { dbDocumentClient } from "../db/client";
+import { QueryCommand } from "@aws-sdk/lib-dynamodb";
+import { productsTableName } from "../constants";
 
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   try {
-    const product = products.find(
-      (prod) => prod.id === event.pathParameters?.productId
-    );
+    const { Items: products } = (await dbDocumentClient.send(
+      new QueryCommand({
+        TableName: productsTableName,
+        KeyConditionExpression: "id = :id",
+        ExpressionAttributeValues: {
+          ":id": event.pathParameters?.productId,
+        },
+      })
+    )) as MyQueryCommandOutput<Product[]>;
 
-    if (!product) {
+    if (!products?.length) {
       throw new HttpError(404, "Product not found");
     }
 
-    return buildResponse(200, product);
+    const product = products[0];
+
+    const { Items: stocks } = (await dbDocumentClient.send(
+      new QueryCommand({
+        TableName: process.env.STOCKS_TABLE_NAME,
+        KeyConditionExpression: "product_id = :product_id",
+        ExpressionAttributeValues: {
+          ":product_id": event.pathParameters?.productId,
+        },
+      })
+    )) as MyQueryCommandOutput<Stock[]>;
+
+    const stock = stocks?.find(
+      (stock) => stock.product_id === event.pathParameters?.productId
+    );
+
+    // DynamoDB doesn't support joins
+
+    const availableProduct = {
+      ...product,
+      count: stock?.count || 0,
+    } satisfies PopulatedProduct;
+
+    return buildResponse(200, availableProduct);
   } catch (error: unknown) {
     const statusCode = error instanceof HttpError ? error.statusCode : 500;
 
--- product-service/lib/handlers/getProductsList.ts
+++ product-service/lib/handlers/getProductsList.ts
@@ -1,15 +1,47 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-
-import { products } from "../db/db";
+import { ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { dbDocumentClient } from "../db/client";
 import { buildResponse } from "../utils";
 import { HttpError } from "../errorHandler";
+import { productsTableName, stocksTableName } from "../constants";
+import type { MyScanCommandOutput } from "../db/types";
+import type { Product, Stock } from "../types";
+
+// TODO add env vars for table names, etc
 
 export const handler = async (
   event?: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   try {
-    return buildResponse(200, products);
-  } catch (error: unknown) {
+    const { Items: productItems } = (await dbDocumentClient.send(
+      new ScanCommand({
+        TableName: productsTableName,
+      })
+    )) as MyScanCommandOutput<Product[]>;
+
+    const { Items: stockItems } = (await dbDocumentClient.send(
+      new ScanCommand({
+        TableName: stocksTableName,
+      })
+    )) as MyScanCommandOutput<Stock[]>;
+
+    const availableProducts = productItems ?? [];
+    const availableStocks = stockItems ?? [];
+
+    // DynamoDB doesn't support joins
+
+    const resultProducts = availableProducts.map((product) => {
+      const stock = availableStocks.find(
+        (stock) => stock.product_id === product.id
+      );
+      return {
+        ...product,
+        stock: stock?.count ?? 0,
+      };
+    });
+
+    return buildResponse(200, resultProducts);
+  } catch (error) {
     const statusCode = error instanceof HttpError ? error.statusCode : 500;
 
     return buildResponse(statusCode, {
