eef0de0d4dea1f8ec4fbfca741f6c1a62f68a681
Eugene Gatsak <greenglaz@gmail.com>
2024-06-20 00:15:19 +0200

    feat: implement s3 bucket & handlers

---
+++ import-service/lib/handlers/importFileParser.ts
@@ -0,0 +1,56 @@
+import { Readable } from "node:stream";
+import { S3Event } from "aws-lambda";
+import {
+  CopyObjectCommand,
+  DeleteObjectCommand,
+  GetObjectCommand,
+} from "@aws-sdk/client-s3";
+import { parse } from "csv-parse";
+import { buildResponse } from "../utils";
+import { s3Client } from "../client";
+
+export const handler = async (event: S3Event) => {
+  console.log("importFileParser: ", event.Records);
+
+  try {
+    const s3File = event.Records[0].s3;
+    const getObjectCommandOutput = await s3Client.send(
+      new GetObjectCommand({
+        Bucket: s3File.bucket.name,
+        Key: s3File.object.key,
+      })
+    );
+
+    (getObjectCommandOutput.Body as Readable)
+      .pipe(parse())
+      .on("data", (row: string) => {
+        console.log(row);
+      })
+      .on("error", (error: Error) => {
+        console.error("Error parsing CSV:", error);
+      });
+
+    await s3Client.send(
+      new CopyObjectCommand({
+        CopySource: `${s3File.bucket.name}/${s3File.object.key}`,
+        Bucket: s3File.bucket.name,
+        Key: `parsed/${s3File.object.key.split("/").slice(-1)}`,
+      })
+    );
+
+    await s3Client.send(
+      new DeleteObjectCommand({
+        Bucket: s3File.bucket.name,
+        Key: s3File.object.key,
+      })
+    );
+
+    return buildResponse(200, {});
+  } catch (error: any) {
+    const statusCode = error.statusCode ?? 500;
+
+    return buildResponse(statusCode, {
+      message: error instanceof Error ? error.message : "Smth went wrong",
+    });
+  }
+};
---
+++ import-service/lib/handlers/importProductsFile.ts
@@ -0,0 +1,35 @@
+import { APIGatewayEvent } from "aws-lambda";
+import { createPresignedPost } from "@aws-sdk/s3-presigned-post";
+import { s3Client } from "../client";
+import { buildResponse } from "../utils";
+
+export const handler = async function (event: APIGatewayEvent) {
+  console.log("importProductsFile: ", event);
+
+  try {
+    if (!event.queryStringParameters) {
+      return buildResponse(400, { message: "Please provide queryString" });
+    }
+    const { name } = event.queryStringParameters;
+
+    const presignedPost = await createPresignedPost(s3Client, {
+      Bucket: process.env.BUCKET_NAME ?? "",
+      Key: `uploaded/${name}`,
+      Fields: {
+        "Content-Type": "text/csv",
+      },
+      Expires: 120,
+    });
+
+    return buildResponse(200, {
+      uploadUrl: presignedPost.url,
+      fields: presignedPost.fields,
+    });
+  } catch (error: any) {
+    const statusCode = error.statusCode ?? 500;
+
+    return buildResponse(statusCode, {
+      message: error instanceof Error ? error.message : "Smth went wrong",
+    });
+  }
+};
