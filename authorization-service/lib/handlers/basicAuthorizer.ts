a12ea401a1e6b689bb2b86284f4da0e7977da562
Eugene Gatsak <greenglaz@gmail.com>
2024-07-03 00:51:59 +0200

    feat: implement basicAuthorizer lambda

--- authorization-service/lib/handlers/basicAuthorizer.ts
+++ authorization-service/lib/handlers/basicAuthorizer.ts
@@ -1,1 +1,45 @@
-export const handler = async function () {};
+import {APIGatewayAuthorizerResult, APIGatewayTokenAuthorizerEvent} from "aws-lambda";
+
+export const handler = function (event: APIGatewayTokenAuthorizerEvent): APIGatewayAuthorizerResult {
+  console.log(`Authorizer event: ${event}`);
+
+  const token = event.authorizationToken.split(" ")[1];
+
+  const decodedToken = Buffer.from(token, "base64").toString("utf-8");
+
+  const [username, password] = decodedToken.split(":");
+
+  const isVerified = username === "egatsak" && password === process.env.egatsak;
+
+  if (isVerified) {
+    console.log("Access granted to user=" + username);
+    return {
+      principalId: username,
+      policyDocument: {
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Action: "execute-api:Invoke",
+            Effect: "Allow",
+            Resource: "*"
+          }
+        ]
+      }
+    } satisfies APIGatewayAuthorizerResult;
+  } else {
+    console.log("Access denied to user=" + username);
+    return {
+      principalId: username,
+      policyDocument: {
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Action: "execute-api:Invoke",
+            Effect: "Deny",
+            Resource: "*"
+          }
+        ]
+      }
+    } satisfies APIGatewayAuthorizerResult;
+  }
+};
