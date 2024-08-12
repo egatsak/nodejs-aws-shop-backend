40ebba81930e0397eb6a2b0e82fc31b7d86b3b74
Eugene Gatsak <greenglaz@gmail.com>
2024-08-12 09:17:32 +0200

    fix: enable cors for bff api

--- bff-service/src/main.ts
+++ bff-service/src/main.ts
@@ -5,6 +5,9 @@ import 'dotenv/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('bff');
+  app.enableCors({
+    origin: (req, callback) => callback(null, true),
+  });
   await app.listen(process.env.PORT);
 }
 bootstrap();
